15df0392a55beb96ff265d8ef40270fe2570f401
devMuscle <hongsbien@naver.com>
2022-02-22 23:46:23 +0900

    resolve conflict

--- src/test/java/com/bb/app/member/MemberRepositoryTest.java
+++ src/test/java/com/bb/app/member/MemberRepositoryTest.java
@@ -1,45 +1,23 @@
 package com.bb.app.member;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.apache.catalina.authenticator.SavedRequest;
+import com.bb.app.Mapper.MemberMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.bb.app.entity.MemberEntity;
 import com.bb.app.repository.MemberRepository;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-
 @SpringBootTest
+@Slf4j
 class MemberRepositoryTest {
 
     @Autowired
     MemberRepository repository;
-    Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    MemberMapper memberMapper;
 
-    @Test
-    @Transactional
-    @Rollback(value = false)
-    void test() {
-       String testid = "testId";
-       String testpass = "testpass5";
-
-       Optional<MemberEntity> m = repository.findByLoginIdAndPassword(testid, testpass);
-
-       MemberEntity member = m.get();
-
-       String a = member.getNickName();
-
-       logger.info(member.getEmail());
-
-    }
     @Test
     @Transactional
     @Rollback(value = false)
