6334e151cf39af75f002d2e4638eb079fdc179e2
molamolac <k9077900@naver.com>
2022-02-22 14:34:35 +0900

    맵버엔티티, 댓글엔티티 관계설정 추가 / 게시판엔티티 LIST업데이트 기능 추가

--- src/main/java/com/bb/app/Mapper/VoteReplyMapper.java
+++ src/main/java/com/bb/app/Mapper/VoteReplyMapper.java
@@ -1,15 +1,24 @@
 package com.bb.app.Mapper;
 
 import com.bb.app.DTO.VoteBoardDto;
+import com.bb.app.DTO.VoteReplyDto;
 import com.bb.app.entity.VoteBoardEntity;
+import com.bb.app.entity.VoteReplyEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface VoteReplyMapper {
     VoteReplyMapper INSTANCE = Mappers.getMapper(VoteReplyMapper.class);
 
+    @Mapping(source = "voteBoardId", target = "vote.board.id")
+    @Mapping(source = "memberId", target = "member.id")
+    VoteReplyEntity toEntity(VoteReplyDto voteReplyDto);
+    VoteReplyDto toDto(VoteReplyEntity voteReplyEntity);
 
-    VoteBoardEntity toEntity(VoteBoardDto voteBoardDto);
-    VoteBoardDto toDto(VoteBoardEntity voteBoardEntity);
+    List<VoteReplyEntity> toEntity(List<VoteReplyDto> voteReplyDto);
+    List<VoteReplyDto> toDto(List<VoteReplyEntity> voteReplyEntity);
 }
--- src/main/java/com/bb/app/service/VoteReplyService.java
+++ src/main/java/com/bb/app/service/VoteReplyService.java
@@ -1,37 +1,43 @@
 package com.bb.app.service;
 
+import com.bb.app.DTO.VoteReplyDto;
+import com.bb.app.Mapper.VoteReplyMapper;
 import com.bb.app.entity.VoteReplyEntity;
 import com.bb.app.repository.VoteReplyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 public class VoteReplyService {
 
-    @Autowired
-    VoteReplyRepository repository;
 
-    public void writeBoardReply(VoteReplyEntity voteReply) {
-        repository.save(voteReply);
+    final VoteReplyRepository repository;
+    public VoteReplyService(VoteReplyRepository repository){
+        this.repository = repository;
+    }
+    @Transactional
+    public void writeBoardReply(VoteReplyDto reply) {
+        VoteReplyEntity replyEntity = VoteReplyMapper.INSTANCE.toEntity(reply);
     }
 
-    public VoteReplyEntity FindCommentById(long id) {
-        Optional<VoteReplyEntity> b = repository.findById(id);
-        VoteReplyEntity voteReply = b.get();
-
-        return voteReply;
-    }
-
-    public Optional<VoteReplyEntity> UpdateBoardReply(VoteReplyEntity voteReply) {
-        repository.save(voteReply);
-        Optional<VoteReplyEntity> updatedVoteReply = repository.findById(voteReply.getId());
-
-        return updatedVoteReply;
-    }
-
-    public void DeleteVoteReply(long replyNo) {
-        repository.deleteById(replyNo);
-    }
+//    public VoteReplyEntity FindCommentById(long id) {
+//        Optional<VoteReplyEntity> b = repository.findById(id);
+//        VoteReplyEntity voteReply = b.get();
+//
+//        return voteReply;
+//    }
+//
+//    public Optional<VoteReplyEntity> UpdateBoardReply(VoteReplyEntity voteReply) {
+//        repository.save(voteReply);
+//        Optional<VoteReplyEntity> updatedVoteReply = repository.findById(voteReply.getId());
+//
+//        return updatedVoteReply;
+//    }
+//
+//    public void DeleteVoteReply(long replyNo) {
+//        repository.deleteById(replyNo);
+//    }
 }
