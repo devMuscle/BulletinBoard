11665fc91fa207f2366b2b12d554703cd4b4abe5
devMuscle <hongsbien@naver.com>
2022-02-22 23:40:37 +0900

    일반 게시판 댓글 관련 기능 수정

--- src/main/java/com/bb/app/service/BoardReplyService.java
+++ src/main/java/com/bb/app/service/BoardReplyService.java
@@ -1,38 +1,40 @@
 package com.bb.app.service;
 
+import com.bb.app.DTO.SaveBoardReplyDto;
+import com.bb.app.Mapper.SaveBoardReplyMapper;
 import com.bb.app.entity.BoardReplyEntity;
+import com.bb.app.exception.DeleteException;
 import com.bb.app.repository.BoardReplyRepository;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class BoardReplyService {
-    @Autowired
-    BoardReplyRepository repository;
 
-    public void writeBoardReply(BoardReplyEntity boardReply) {
-        repository.save(boardReply);
+    private final BoardReplyRepository boardReplyRepository;
+    private final SaveBoardReplyMapper saveBoardReplyMapper;
+
+    public void writeBoardReply(SaveBoardReplyDto boardReplyDto) {
+        BoardReplyEntity boardReplyEntity = saveBoardReplyMapper.toEntity(boardReplyDto);
+        boardReplyRepository.save(boardReplyEntity);
     }
 
-    public BoardReplyEntity FindCommentById(long id) {
-        Optional<BoardReplyEntity> b = repository.findById(id);
-        BoardReplyEntity boardReply = b.get();
-        return boardReply;
+    public void UpdateBoardReply(long no, SaveBoardReplyDto boardReplyDto) {
+       Optional<BoardReplyEntity> opBoardReplyEntity = boardReplyRepository.findById(no);
+       BoardReplyEntity boardReplyEntity = opBoardReplyEntity.orElseThrow();
+       boardReplyEntity.updateComment(boardReplyDto.getComment());
     }
 
-    public Optional<BoardReplyEntity> UpdateBoardReply(BoardReplyEntity boardReply) {
-        repository.save(boardReply);
-        Optional<BoardReplyEntity> updatedBoardReply = repository.findById(boardReply.getId());
-
-        return updatedBoardReply;
-    }
-
-    public void DeleteBoardReply(long replyNo) {
-        repository.deleteById(replyNo);
+    public void DeleteBoardReply(long replyNo) throws DeleteException {
+        boardReplyRepository.deleteById(replyNo);
     }
 }
