a9274a5ff2c05fd2b75a6c6c3fae525c5981a20c
devMuscle <hongsbien@naver.com>
2022-02-23 22:28:00 +0900

    parameter Annotation 수정

--- src/main/java/com/bb/app/controller/BoardController.java
+++ src/main/java/com/bb/app/controller/BoardController.java
@@ -5,8 +5,11 @@ import com.bb.app.DTO.VoteBoardDto;
 import com.bb.app.entity.BoardEntity;
 import com.bb.app.entity.VoteBoardEntity;
 import com.bb.app.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,24 +17,33 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.*;
 
 @RestController
 @RequestMapping("/boards")
+@Log4j2
+@RequiredArgsConstructor
 public class BoardController {
-    @Autowired
-    BoardService service;
 
-    Logger logger = LoggerFactory.getLogger(getClass());
+    private final BoardService service;
 
     //trade 게시판 컨트롤
-    @PostMapping("/trade-board/write")
-    public ResponseEntity<Void> TradeboardWrite(@RequestBody BoardDto board){
-        service.writeBoard(board);
-
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    @PostMapping(value = "/trade-board/write")
+    public ResponseEntity<Void> TradeboardWrite(@RequestPart(required = false) List<MultipartFile> files, @RequestPart BoardDto board){
+        try{
+            log.info("id : {}", board.getMember());
+            log.info("content : {}", board.getContent());
+        //    log.info("files : {}", files.get(0).getOriginalFilename());
+            service.writeBoard(files, board);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
     @GetMapping("/trade-board/{bno}")
     public BoardDto TradeboardView(@PathVariable long bno){
         BoardDto board = service.TradeboardView(bno);
